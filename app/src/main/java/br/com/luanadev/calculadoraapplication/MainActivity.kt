2c3bc517a89b124ba3bf2adca05166f0013eb6b6
Luana Souza <luana.souza@ifood.com.br>
2021-12-20 09:31:16 -0300

    refatorando Ids

--- app/src/main/java/br/com/luanadev/calculadoraapplication/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/calculadoraapplication/MainActivity.kt
@@ -24,15 +24,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setResult() {
-        igual.setOnClickListener {
+        equal.setOnClickListener {
             try {
                 val expressao = ExpressionBuilder(expressao.text.toString()).build()
                 val resultado = expressao.evaluate()
                 val longResult = resultado.toLong()
                 if (resultado == longResult.toDouble()) {
-                    txt_resultado.text = longResult.toString()
+                    result.text = longResult.toString()
                 } else {
-                    txt_resultado.text = resultado.toString()
+                    result.text = resultado.toString()
                 }
 
             } catch (e: Exception) {
@@ -46,50 +46,50 @@ class MainActivity : AppCompatActivity() {
             if (textoDigitado.isNotBlank()) {
                 expressao.text = textoDigitado.substring(0, textoDigitado.length - 1)
             }
-            txt_resultado.text = ""
+            result.text = ""
         }
     }
 
     private fun setLimpar() {
-        limpar.setOnClickListener {
+        clean.setOnClickListener {
             expressao.text = ""
-            txt_resultado.text = ""
+            result.text = ""
         }
     }
 
     private fun setOperadores() {
-        soma.setOnClickListener { acrescentarUmaExpressao("+", false) }
-        subitracao.setOnClickListener { acrescentarUmaExpressao("-", false) }
-        multiplicacao.setOnClickListener { acrescentarUmaExpressao("*", false) }
-        divisao.setOnClickListener { acrescentarUmaExpressao("/", false) }
+        add_up.setOnClickListener { acrescentarUmaExpressao("+", false) }
+        subtraction.setOnClickListener { acrescentarUmaExpressao("-", false) }
+        multiplication.setOnClickListener { acrescentarUmaExpressao("*", false) }
+        division.setOnClickListener { acrescentarUmaExpressao("/", false) }
     }
 
     private fun setNumber() {
-        numero_zero.setOnClickListener { acrescentarUmaExpressao("0", true) }
-        numero_um.setOnClickListener { acrescentarUmaExpressao("1", true) }
-        numero_dois.setOnClickListener { acrescentarUmaExpressao("2", true) }
-        numero_tres.setOnClickListener { acrescentarUmaExpressao("3", true) }
-        numero_quatro.setOnClickListener { acrescentarUmaExpressao("4", true) }
-        numero_cinco.setOnClickListener { acrescentarUmaExpressao("5", true) }
-        numero_seis.setOnClickListener { acrescentarUmaExpressao("6", true) }
-        numero_sete.setOnClickListener { acrescentarUmaExpressao("7", true) }
-        numero_oito.setOnClickListener { acrescentarUmaExpressao("8", true) }
-        numero_nove.setOnClickListener { acrescentarUmaExpressao("9", true) }
+        number_zero.setOnClickListener { acrescentarUmaExpressao("0", true) }
+        number_one.setOnClickListener { acrescentarUmaExpressao("1", true) }
+        number_two.setOnClickListener { acrescentarUmaExpressao("2", true) }
+        number_three.setOnClickListener { acrescentarUmaExpressao("3", true) }
+        number_four.setOnClickListener { acrescentarUmaExpressao("4", true) }
+        number_five.setOnClickListener { acrescentarUmaExpressao("5", true) }
+        number_six.setOnClickListener { acrescentarUmaExpressao("6", true) }
+        number_seven.setOnClickListener { acrescentarUmaExpressao("7", true) }
+        number_eight.setOnClickListener { acrescentarUmaExpressao("8", true) }
+        number_nine.setOnClickListener { acrescentarUmaExpressao("9", true) }
         ponto.setOnClickListener { acrescentarUmaExpressao(".", true) }
 
     }
 
     private fun acrescentarUmaExpressao(dados: String, limpar_dados: Boolean) {
-        if (txt_resultado.text.isNotEmpty()) {
+        if (result.text.isNotEmpty()) {
             expressao.text = ""
         }
         if (limpar_dados) {
-            txt_resultado.text = ""
+            result.text = ""
             expressao.append(dados)
         } else {
-            expressao.append(txt_resultado.text)
+            expressao.append(result.text)
             expressao.append(dados)
-            txt_resultado.text = ""
+            result.text = ""
         }
     }
 
