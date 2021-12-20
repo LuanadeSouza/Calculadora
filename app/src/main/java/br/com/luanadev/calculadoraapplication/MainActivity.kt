37814c8fb3aae3d045632d829f6519971ecbf31e
Luana Souza <luana.souza@ifood.com.br>
2021-12-20 09:55:45 -0300

    refatorando parameters names

--- app/src/main/java/br/com/luanadev/calculadoraapplication/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/calculadoraapplication/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private fun init() {
         setNumber()
         setOperators()
-        setClear()
+        setClean()
         setDelete()
         setExpectedResult()
     }
@@ -26,13 +26,13 @@ class MainActivity : AppCompatActivity() {
     private fun setExpectedResult() {
         equal.setOnClickListener {
             try {
-                val expressao = ExpressionBuilder(expression.text.toString()).build()
-                val resultado = expressao.evaluate()
-                val longResult = resultado.toLong()
-                if (resultado == longResult.toDouble()) {
+                val expectedExpression = ExpressionBuilder(expression.text.toString()).build()
+                val expectedResult = expectedExpression.evaluate()
+                val longResult = expectedResult.toLong()
+                if (expectedResult == longResult.toDouble()) {
                     result.text = longResult.toString()
                 } else {
-                    result.text = resultado.toString()
+                    result.text = expectedResult.toString()
                 }
 
             } catch (e: Exception) {
@@ -42,15 +42,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun setDelete() {
         backspace.setOnClickListener {
-            val textoDigitado = expression.text.toString()
-            if (textoDigitado.isNotBlank()) {
-                expression.text = textoDigitado.substring(0, textoDigitado.length - 1)
+            val typedtext = expression.text.toString()
+            if (typedtext.isNotBlank()) {
+                expression.text = typedtext.substring(0, typedtext.length - 1)
             }
             result.text = ""
         }
     }
 
-    private fun setClear() {
+    private fun setClean() {
         clean.setOnClickListener {
             expression.text = ""
             result.text = ""
@@ -58,37 +58,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOperators() {
-        add_up.setOnClickListener { acrescentarUmaExpressao("+", false) }
-        subtraction.setOnClickListener { acrescentarUmaExpressao("-", false) }
-        multiplication.setOnClickListener { acrescentarUmaExpressao("*", false) }
-        division.setOnClickListener { acrescentarUmaExpressao("/", false) }
+        add_up.setOnClickListener { makeAnExpression("+", false) }
+        subtraction.setOnClickListener { makeAnExpression("-", false) }
+        multiplication.setOnClickListener { makeAnExpression("*", false) }
+        division.setOnClickListener { makeAnExpression("/", false) }
     }
 
     private fun setNumber() {
-        number_zero.setOnClickListener { acrescentarUmaExpressao("0", true) }
-        number_one.setOnClickListener { acrescentarUmaExpressao("1", true) }
-        number_two.setOnClickListener { acrescentarUmaExpressao("2", true) }
-        number_three.setOnClickListener { acrescentarUmaExpressao("3", true) }
-        number_four.setOnClickListener { acrescentarUmaExpressao("4", true) }
-        number_five.setOnClickListener { acrescentarUmaExpressao("5", true) }
-        number_six.setOnClickListener { acrescentarUmaExpressao("6", true) }
-        number_seven.setOnClickListener { acrescentarUmaExpressao("7", true) }
-        number_eight.setOnClickListener { acrescentarUmaExpressao("8", true) }
-        number_nine.setOnClickListener { acrescentarUmaExpressao("9", true) }
-        ponto.setOnClickListener { acrescentarUmaExpressao(".", true) }
+        number_zero.setOnClickListener { makeAnExpression("0", true) }
+        number_one.setOnClickListener { makeAnExpression("1", true) }
+        number_two.setOnClickListener { makeAnExpression("2", true) }
+        number_three.setOnClickListener { makeAnExpression("3", true) }
+        number_four.setOnClickListener { makeAnExpression("4", true) }
+        number_five.setOnClickListener { makeAnExpression("5", true) }
+        number_six.setOnClickListener { makeAnExpression("6", true) }
+        number_seven.setOnClickListener { makeAnExpression("7", true) }
+        number_eight.setOnClickListener { makeAnExpression("8", true) }
+        number_nine.setOnClickListener { makeAnExpression("9", true) }
+        ponto.setOnClickListener { makeAnExpression(".", true) }
 
     }
 
-    private fun acrescentarUmaExpressao(dados: String, limpar_dados: Boolean) {
+    private fun makeAnExpression(data: String, clearData: Boolean) {
         if (result.text.isNotEmpty()) {
             expression.text = ""
         }
-        if (limpar_dados) {
+        if (clearData) {
             result.text = ""
-            expression.append(dados)
+            expression.append(data)
         } else {
             expression.append(result.text)
-            expression.append(dados)
+            expression.append(data)
             result.text = ""
         }
     }
